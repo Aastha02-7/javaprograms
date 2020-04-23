87b9901898844f4e5f099441a1fddb84539b930e
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2020-04-23 14:21:45 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class multiplication{
+public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		float number1 = inp.nextFloat();
+		float num2 = inp.nextFloat();
+		float num3 ;
+		num3 = number1  + num2;
+		System.out.print("ADDITION = "+num3);
+		
+	}
+}
+
+    

2bb2d1f9e65cf597425eb1ba4d73b03d9bb570ff
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2020-04-23 17:59:08 +0530

    Update simpleinterest.java

--- simpleinterest.java
+++ simpleinterest.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
 
-/**
- *
- * @author aasth
- */
-public class simpleinterest {
-    
+public class interest {
+	public static void main (String [] args) {
+		 Scanner ip= new Scanner(System.in);
+		  	float  p =ip.nextFloat();
+		 	float  r =ip.nextFloat();
+		 	float  t =ip.nextFloat();
+		 	float i;
+		 	i = (p*r*t)/100;
+		 	System.out.print("Interest is : "+ i);
+	}
 }

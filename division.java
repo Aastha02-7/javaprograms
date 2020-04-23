d02a5050a1b835b6e6112d81eccdb88e032425c0
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2020-04-23 14:26:26 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class division {
+	public static void main(String [] args) {
+		Scanner d = new Scanner(System.in);
+		System.out.print("Enter first number :");
+		float a = d.nextFloat();
+		System.out.print("Enter second number :");
+		float b = d.nextFloat();
+		float c;
+		c= a/b;
+		System.out.print("a =" +a+ "\nb = "+b+"\nDivision = "+ c);
+	}
+
+}
+    
+

67a1658ab0a52733be9a494d46c85f670165fae6
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2020-04-23 14:08:31 +0530

    Add files via upload

---
+++ lowercase.java
@@ -0,0 +1,12 @@
+public class lowercase {
+	public static void main(String[] args) {
+		String a;
+		String z;
+		a = new String("AASTHA HARINKHERE");
+		z = a.toLowerCase();
+		System.out.print(a + "\nConvert Lowercase \n"+z);
+	}
+
+}
+    
+

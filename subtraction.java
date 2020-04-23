b77125220606d826457217f53b87d22e32b175cc
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2020-04-23 18:01:29 +0530

    Update subtraction.java

--- subtraction.java
+++ subtraction.java
@@ -1,13 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author aasth
- */
+import java.util.Scanner;
 public class subtraction {
-    
+	public static void main(String [] args) {
+		Scanner d = new Scanner(System.in);
+		System.out.print("Enter first number :");
+		float a = d.nextFloat();
+		System.out.print("Enter second number :");
+		float b = d.nextFloat();
+		float c;
+		c= a-b;
+		System.out.print("a =" +a+ "\nb = "+b+"\nSubtraction = "+ c);
+	}
+
 }

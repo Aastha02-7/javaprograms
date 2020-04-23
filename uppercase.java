3a0eba4aed383549044e57878b5a6822a9a2e4f9
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2020-04-23 13:48:44 +0530

    Add files via upload

---
+++ uppercase.java
@@ -0,0 +1,13 @@
+
+public class uppercase {
+	public static void main(String[] args) {
+		String a;
+		String z;
+		a = "aastha harinkhere";
+		z = a.toUpperCase();
+		System.out.print(a + "\nConvert uppercase \n"+z);
+	}
+
+}
+    
+
